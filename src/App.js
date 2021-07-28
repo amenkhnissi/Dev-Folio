5a3b74a4a6854452f30ec59dfb6dd1de2d05db23
Amen <amenkhnissi@gmail.com>
2021-07-28 10:33:57 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+function App() {
+  return (
+   <Navbar />
+  );
+}
+
+export default App;
